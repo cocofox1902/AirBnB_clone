a5f64312515ec6e09dcf1870c2b37362b88bedd2
cocofox1902 <colas.renard1902@gmail.com>
2022-02-28 13:27:11 +0000

    add all

--- console.py
+++ console.py
@@ -1,8 +1,34 @@
 #!/usr/bin/python3
 import cmd
+from models import base_model
+BaseModel = base_model.BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def default(self, line):
+        """nothing to print"""
+        pass
+
+    def emptyline(self):
+        """empty line"""
+        pass
+
+    def do_quit(self, line):
+        """Quit command to exit the program
+        """
+        quit()
+
+    def do_EOF(self, line):
+        """End Of File"""
+        quit()
+
+    def do_create(self, line):
+        """
+        """
+        print(my_obj.id)
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """
+The class FileStorage is a class that define the new dictionary of the class
 """
 import json
 from datetime import datetime
@@ -8,25 +9,54 @@ from models import base_model
 
 class FileStorage:
     """
+    All the instance used listed:
+    all(self):
+    new(self, obj):
+    save(self):
+    reload(self):
     """
 
     __file_path = './file.json'
     __objects = {}
 
-
     def all(self):
         """
+        Function - all(self):
+            return the object
+
+            Object:
+                nothing
+
+            Return:
+                object
         """
         return(FileStorage.__objects)
 
     def new(self, obj):
         """
+        Function - new(self, obj):
+            add new object to the dict
+
+            Object:
+                copy(str): the dictionary of the file
+
+            Return:
+                nothing
         """
         copy = "{}.{}".format(type(obj).__name__, obj.id)
         FileStorage.__objects[copy] = obj
 
     def save(self):
         """
+        Function - save(self):
+            save the new dictionary
+
+            Object:
+                file(str): the name of wherethe file is
+                dict3(dict): the new dictionary
+
+            Return:
+                nothing
         """
         file = FileStorage.__file_path
         dict3 = {}
@@ -37,6 +67,15 @@ class FileStorage:
 
     def reload(self):
         """
+        Function - reload(self):
+            reload a new dictionary
+
+            Object:
+                file(str): the name of wherethe file is
+                FileStorage.__objects(dict): the new dictionary
+
+            Return:
+                nothing
         """
         file = FileStorage.__file_path
         FileStorage.__objects = {}
